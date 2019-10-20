813135cbd01e879ee37cb28efa93829100e829e4
Rafael Garcia <1337technomage@gmail.com>
2019-10-20 00:41:36 -0400

    Initial Commit

---
+++ WREK Formatter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WREK Formatter
+//
+//  Created by David Garcia on 10/20/19.
+//  Copyright © 2019 Ayy Lmao LLC. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
