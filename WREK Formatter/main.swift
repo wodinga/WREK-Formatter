8b5323b2b1cfc87e84d462320e00e1562633c588
David Garcia <david_r_garcia@homedepot.com>
2019-10-21 12:30:06 -0400

    Code to take a mix that is longer than an hour and cut it up into chunks

--- WREK Formatter/main.swift
+++ WREK Formatter/main.swift
@@ -7,6 +7,66 @@
 //
 
 import Foundation
+import AVFoundation
 
-print("Hello, World!")
+let manager = FileManager()
+let desktopPath = NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true)[0]
 
+//let date = Date()
+let cal = Calendar.current
+let nextWeekend = cal.nextWeekend(startingAfter: Date())!
+let dateFormatter = DateFormatter()
+dateFormatter.locale = .current
+dateFormatter.dateFormat = "MMdd"
+let dateString = dateFormatter.string(from: nextWeekend.start)
+guard CommandLine.arguments.count == 2,
+manager.fileExists(atPath: CommandLine.arguments[1]) else {
+    print("file does not exist or incorrect number of args")
+    exit(EXIT_FAILURE)
+}
+
+let url = URL(fileURLWithPath: CommandLine.arguments[1])
+let asset = AVAsset(url: url)
+assert(asset.tracks[0].mediaType == .audio)
+
+let fileLength = CMTime(value: asset.duration.value, timescale: asset.duration.timescale)
+print( "\(fileLength.seconds/60) minutes" )
+
+var start: CMTime = CMTime(seconds: 0, preferredTimescale: fileLength.timescale)
+var duration: CMTime = CMTime(seconds: (fileLength.seconds/60 >= 60) ? 59 * 60 + 59 : fileLength.seconds, preferredTimescale: fileLength.timescale)
+print(start)
+print(duration)
+let group = DispatchGroup()
+var index = 0
+
+repeat {
+    index += 1
+    let exportSession = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetPassthrough)
+    duration = (fileLength.value < start.value + duration.value) ?
+        CMTime(value: fileLength.value - start.value, timescale: fileLength.timescale) : duration
+    exportSession?.outputURL = URL(fileURLWithPath: "EDM\(index)_\(dateString).aiff",
+        relativeTo: URL(fileURLWithPath: desktopPath, isDirectory: true))
+    exportSession?.outputFileType = .aiff
+    exportSession?.timeRange = CMTimeRangeMake(start: start,
+                                               duration: duration)
+    group.enter()
+    exportSession?.exportAsynchronously {
+        if exportSession!.error == nil {
+            switch exportSession!.status {
+            case .completed:
+                print(exportSession!.outputURL!.absoluteString)
+
+            default:
+                print(exportSession!.status)
+            }
+
+        } else {
+            print(exportSession!.error.debugDescription)
+        }
+        group.leave()
+    }
+    start = start + duration
+}while(start < fileLength)
+
+group.wait()
+print("Finished exporting!")
